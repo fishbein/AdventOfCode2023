2d615f0a17de9f869fea70cb684c309e445876cb
Phil Fishbein <phil@fishbe.in>
2023-11-29 17:32:46 -0500

    Initial Commit

---
+++ AdventOfCode2023/AdventOfCode2023App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2023App.swift
+//  AdventOfCode2023
+//
+//  Created by Phil Fishbein on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
